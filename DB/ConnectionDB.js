8a06063d2bfbf28c1c34cb7bb31492c58abd3e15
Shaaban <shabanhamdy94@gmail.com>
2023-06-19 18:53:04 +0300

    first commit

--- DB/ConnectionDB.js
+++ DB/ConnectionDB.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const ConnectionDB = async () => {
-    return await mongoose.connect(process.env.CONNECTION_ONLINE)
+    return await mongoose.connect("mongodb+srv://shabanhamdy94:MdKDd2VQUsnuic1T@cluster0.r60xmyb.mongodb.net/CRUDSYSTEM")
         .then((res) => console.log('Connection DB is Running........'))
         .catch((err) => console.log(`Connection DB error.${err}`))
 }
